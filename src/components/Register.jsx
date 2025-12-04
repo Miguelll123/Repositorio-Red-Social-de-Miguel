a4a56f0b56a6e2c925833d0f1d23b81cba4e0473
Miguelll123 <mrubiosanfeliu@gmail.com>
2025-12-04 17:39:55 +0100

    Register ok

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -2,13 +2,12 @@ import React,{useEffect, useState} from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { register, reset } from '../features/authSlice';
 import {notification} from 'antd';
+import './Register.css';
+import { Form, Input, Button, Upload } from 'antd';
 
 const Register = ()=> {
     const [formData,setFormData] = useState({
-        username:'',
-        email:'',
-        password:'',
-        password2:'',
+        
         image: null // Para guardar el archivo de imagen
     });
 
@@ -45,9 +44,8 @@ const Register = ()=> {
    }
   }
 
-  const onSubmit = (e) => {
-    e.preventDefault();
-    if(password !== password2){
+  const onFinish = (values) => {
+    if(values.password !== values.password2){
         return notification.error({
             message:'Error',
             description:"Passwords do not match",
@@ -55,9 +53,9 @@ const Register = ()=> {
     } else {
     // Crear FormData para enviar multipart/form-data
     const formDataToSend = new FormData();
-    formDataToSend.append('username', username);
-    formDataToSend.append('email', email);
-    formDataToSend.append('password', password);
+    formDataToSend.append('username', values.username);
+    formDataToSend.append('email', values.email);
+    formDataToSend.append('password', values.password);
     // Solo añadir la imagen si existe
     if(image){
       formDataToSend.append('image', image);
@@ -69,21 +67,22 @@ const Register = ()=> {
   };
 
   return (
-    <form onSubmit={onSubmit}>
-      <input type='text' name='username' value={username} onChange={onChange} placeholder='Username' />
-      <input type='email' name='email' value={email} onChange={onChange} placeholder='Email' />
-      <input type='password' name='password' value={password} onChange={onChange} placeholder='Password' />
-      <input type='password' name='password2' value={password2} onChange={onChange} placeholder='Confirm Password'/>
+    
+    <Form className='register-container' onFinish={onFinish}>
+      <Form.Item  name='username' ><Input placeholder='Username'/></Form.Item>
+      <Form.Item  name='email'><Input placeholder='email'/></Form.Item>
+      <Form.Item name='password'><Input placeholder='Password'/></Form.Item>
+      <Form.Item  name='password2'><Input placeholder='password 2'/></Form.Item>
       {/* Input para subir imagen */}
-      <input 
-        type='file' 
-        name='image' 
-        accept='image/*' 
-        onChange={onChange} 
-      />
-      {image && <p>Imagen seleccionada: {image.name}</p>}
-      <button type='submit'>Register</button>
-    </form>
+      <Form.Item name='image'>
+        <Input type='file' name='image' accept='image/*' onChange={onChange}/></Form.Item>
+      
+      <Button type="primary" htmlType="submit">
+        Submit
+      </Button>
+    </Form>
   )
 }
 export default Register 
+
+
